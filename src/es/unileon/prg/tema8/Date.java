d4ad5ed5ac38dd2667bfe04f717fa379f9838afd
mfernl <mfernl20@estudiantes.unileon.es>
2021-11-13 19:12:07 +0100

    hecho hasta el metodo de las estaciones

--- src/es/unileon/prg/tema8/Date.java
+++ src/es/unileon/prg/tema8/Date.java
@@ -61,7 +61,78 @@ public class Date {
 	}
 	
 	public String toString() {
-		return this.day + "/" + this.month + "/" + this.year;
+		return this.day+"/"+this.month+"/"+this.year;
 	}
-
+	public boolean isSameYear(int year){
+		if(this.year==year){return true;}
+		else{return false;}
+	}
+	public boolean isSameMonth(int month){
+		if(this.month==month){return true;}
+		else{return false;}
+	}
+	public boolean isSameDay(int day){
+		if(this.day==day){return true;}
+		else{return false;}
+	}
+	public boolean isSame(String date){
+		if(date==toString()){return true;}
+		else{return false;}
+	}
+	public String nameMonth (){
+		String uno,dos,tres,cuatro,cinco,seis,siete,ocho,nueve,diez,once,doce;
+		uno = "enero";
+		dos = "febrero";
+		tres = "marzo";
+		cuatro = "abril";
+		cinco = "mayo";
+		seis = "junio";
+		siete = "julio";
+		ocho = "agosto";
+		nueve = "semptiembre";
+		diez = "octubre";
+		once = "noviembre";
+		doce = "diciembre";
+		if(this.month==1){return uno;}
+		if(this.month==2){return dos;}
+		if(this.month==3){return tres;}
+		if(this.month==4){return cuatro;}
+		if(this.month==5){return cinco;}
+		if(this.month==6){return seis;}
+		if(this.month==7){return siete;}
+		if(this.month==8){return ocho;}
+		if(this.month==9){return nueve;}
+		if(this.month==10){return diez;}
+		if(this.month==11){return once;}
+		if(this.month==12){return doce;}
+		else{return dos;}
+	}
+	public boolean isDayRight(){
+		if(this.day>0){
+			if(this.month<getDaysOfMonth()){
+				return true;
+			}
+			else{return false;}
+		}
+		else{ return false;}
+	}
+	public String getEstacion(){
+		if(this.month==1){return  "Invierno";}
+		if(this.month==2){return  "Invierno";}
+		if(this.month==3){if(this.day<20){return  "Invierno";}
+	else{return  "Primavera";}}
+		if(this.month==4){return  "Primavera";}
+		if(this.month==5){return  "Primavera";}
+		if(this.month==6){if(this.day<22){return  "Primavera";}
+		else{return  "Verano";}}
+		if(this.month==7){return "Verano";}
+		if(this.month==8){return  "Verano";}
+		if(this.month==9){if(this.day<23){return  "Verano";}
+		else{return  "Otoño";}}
+		if(this.month==10){return  "Otoño";}
+		if(this.month==11){return  "Otoño";}
+		if(this.month==12){if(this.day<22){return  "Otoño";}
+		else{return  "Invierno";}}
+		else{return "Introduzca un mes valido";}
+	}
 }
--- src/es/unileon/prg/tema8/MainDate.java
+++ src/es/unileon/prg/tema8/MainDate.java
@@ -4,13 +4,21 @@ public class MainDate {
 
 	public static void main(String[] args) {
 		Date today;
-		
 		try {
-			today = new Date(11,11,2020);
+			today = new Date(21,12,2020);
 			System.out.println(today);
+			System.out.println(today.isSameYear(2020));
+			System.out.println(today.isSameMonth(11));
+			System.out.println(today.isSameDay(11));
+			System.out.println(today.isSame("11/11/2021"));
+			System.out.println(today.nameMonth());
+			System.out.println(today.isDayRight());
+			System.out.println(today.getEstacion());
+
 		} catch (DateException e) {
 			System.out.println(e.getMessage());
 		}
+
 	}
 
 }
